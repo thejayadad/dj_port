6a2e708926b405e42d806db809579c4b8e92d2dd
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-04-22 10:32:53 -0400

    update

--- blog_site/blog/urls.py
+++ blog_site/blog/urls.py
@@ -4,4 +4,8 @@ from blog import views
 
 urlpatterns = [
     path("about/", views.AboutView.as_view(), name='about'),
+    path("", views.AboutView.as_view(), name='about'),
+    path("post/(?P<pk>\d+)S", views.PostDetailView.as_view(), name='post_detail'),
+    path("post/new", views.CreatePostView.as_view(), name='post_new'),
+    path("post/(?P<pk>\d+)S/edit/$", views.PostUpdateView.as_view(), name='post_edit')
 ]
--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,9 +1,12 @@
 from datetime import timezone
 from django.shortcuts import render
-from django.views.generic import (TemplateView, ListView
-                                  
-                                  )
+from blog.forms import PostForm, CommentForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic import (TemplateView, ListView,
+                                  DetailView,CreateView, UpdateView, DeleteView)
 from blog.models import Post, Comment
+from django.urls import reverse_lazy
 # Create your views here.
 
 class AboutView(TemplateView):
@@ -15,3 +18,22 @@ class PostListView(ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+    
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
