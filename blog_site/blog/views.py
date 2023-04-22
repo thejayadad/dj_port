0ee87f58d018cfbf5ff98bd84d63d3e3b8383dc8
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-04-22 10:14:26 -0400

    update

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,3 +1,17 @@
+from datetime import timezone
 from django.shortcuts import render
-
+from django.views.generic import (TemplateView, ListView
+                                  
+                                  )
+from blog.models import Post, Comment
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
